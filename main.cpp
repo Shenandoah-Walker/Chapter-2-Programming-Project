fa42049e5bad95b314ac948e354cbb3d7ba89ef8
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-06-22 20:58:18 +0000

    Created Energy Drink Consumption program

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
-//Ocean Levels Program
+//Energy Drink Consumption Program
 
 #include <iostream>
 using namespace std;
 
 int main() 
 {
-  // Create and initialize variables
-  double rateOfIncrease = 1.5;
-  double years1 = 5, years2 = 7, years3 = 10;
-  double levelChange1, levelChange2, levelChange3;
-
-//The level change is calculated by multiplying the rate of increase by the number of years
-  levelChange1 = rateOfIncrease * years1;
-  levelChange2 = rateOfIncrease * years2;
-  levelChange3 = rateOfIncrease * years3;
+  /* Create and initialize variables: surveySize is the number of people surveyed, percentCustomers is the percentage of customers who purchase one or more energy drinks per week, and percentCitrus is the percentage of customers in the group who prefer citrus flavored energy drinks. */
+  double surveySize, percentCustomers, percentCitrus;
+  surveySize = 16500;
+  percentCustomers = surveySize * 0.15; //15% of those surveyed purchase one or more energy drinks per week
+  percentCitrus = percentCustomers * 0.58; // 58% of those surveyed who purchase energy drinks prefer citrus flavored energy drinks.
 
   //Print the results to the console
-  cout << "In " <<  years1 << " years, the ocean's level will be "<< levelChange1 << " millimeters higher than the current level." << endl;
-  cout << "In " <<  years2 << " years, the ocean's level will be "<< levelChange2 << " millimeters higher than the current level." << endl;
-  cout << "In " <<  years3 << " years, the ocean's level will be "<< levelChange3 << " millimeters higher than the current level." << endl;
+  cout << "Approximately " << percentCustomers << " customers in the survey purchase one or more energy drinks per week." << endl;
+  cout << "Approximately " << percentCitrus << " customers in the survey who purchase energy drinks prefer citrus flavored energy drinks." << endl;
 
   return 0;
 }
