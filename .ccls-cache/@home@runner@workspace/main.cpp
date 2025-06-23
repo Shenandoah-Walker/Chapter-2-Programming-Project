e705e989fa3f42acf01a800a5a69c6fafe8ad0ca
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-06-23 16:45:14 +0000

    .

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,17 +1,20 @@
+//Distance per Tank of Gas program
+
 #include <iostream>
 using namespace std;
 
 int main() 
 {
-  // Declare variables for tank size, average miles per gallon when driven in town or on the highway (resectively), and the distance that can be driven in town or on the highway (respectively).
+  /* Declare variables for tank size, average miles per gallon when driven in town or on the highway (resectively), and the distance that can be driven in town or on the highway (respectively). */
   double tankSize, townMileage, highwayMileage, townDistance, highwayDistance; 
-
+  
+// Assign values to the variables.
   tankSize = 20;
   townMileage = 23.5;
   highwayMileage = 28.9;
   townDistance = tankSize * townMileage;
   highwayDistance = tankSize * highwayMileage;
-
-  cout << "The car can travel " << townDistance << " miles in town." << endl;
-  cout << "The car can travel " << highwayDistance << " miles on the highway." << endl;
+// Print the results to the console.
+  cout << "With one full " << tankSize << "-gallon tank, the car can travel " << townDistance << " miles in town." << endl;
+  cout << "With one full " << tankSize << "-gallon tank, the car can travel " << highwayDistance << " miles on the highway." << endl;
 }
